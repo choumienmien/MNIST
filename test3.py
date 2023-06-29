9aaca4b2509d23e757f60bbadeabe963e17c2972
周緬緬 <mienmienchou@gmail.com>
2023-06-29 22:17:45 +0800

    增加備註，然後重新跑對照組模型，test3 是全連接層模型，而 test4 是捲積層+全連接

--- test3.py
+++ test3.py
@@ -28,18 +28,18 @@ def input_pic_txt():
         f = open(TxtName, 'w')
         img_path_Line = os.listdir(img_path)
         for ImgName in img_path_Line:
-            label = "%s" % name  # 修改你标签的名字
+            label = "%s" % name  # 修改你標簽的名字
             Name_Label = img_path + ImgName + ',' + label
             print(Name_Label)
-            f.write(Name_Label)  # 格式化写入也可以
-            f.write('\n')  # 显示写入换行
+            f.write(Name_Label)  # 格式化寫入也可以
+            f.write('\n')  # 顯示寫入換行
         f.close()
 
 
 # 將所有照片0-9寫入的tx，製作all.txt
 def write_txt():
     # -*- coding:UTF-8 -*-
-    # fq = open('./mnist/mnist/all.txt', 'w')  # 这里用追加模式 #訓練
+    # fq = open('./mnist/mnist/all.txt', 'w')  # 這裡用追加模式 #訓練
     fq = open('./mnist/mnist/test_m.txt', 'w')  # 測試
     for i in range(10):
         name = i
@@ -53,20 +53,20 @@ def write_txt():
 
 
 # -*- coding:utf-8 -*-
-# 在txt文件中随机抽取行
+# 在txt文件中隨機抽取行
 def split_train_test():
     All = open('./mnist/mnist/all.txt', 'r', encoding='utf-8')  # 要被抽取的文件All.txt，共63,131行
-    trainf = open('./mnist/mnist/train.txt', 'w', encoding='utf-8')  # 抽取的0.7倍写入train.txt
-    testf = open('./mnist/mnist/test.txt', 'w', encoding='utf-8')  # 抽取的0.3倍写入test.txt
-    AllNum = 69166  # 总图像数
-    SetTrainNum = 0.9  # 设置比例
+    trainf = open('./mnist/mnist/train.txt', 'w', encoding='utf-8')  # 抽取的0.7倍寫入train.txt
+    testf = open('./mnist/mnist/test.txt', 'w', encoding='utf-8')  # 抽取的0.3倍寫入test.txt
+    AllNum = 69166  # 總圖像數
+    SetTrainNum = 0.9  # 設置比例
     SetTestNum = 0.1
 
     trainNum = math.floor(SetTrainNum * AllNum)
     testNum = math.floor(SetTestNum * AllNum)
 
-    trainresultList = random.sample(range(0, AllNum), trainNum)  # sample(x,y)函数的作用是从序列x中，随机选择y个不重复的元素
-    testresultList = random.sample(range(0, AllNum), testNum)  # sample(x,y)函数的作用是从序列x中，随机选择y个不重复的元素
+    trainresultList = random.sample(range(0, AllNum), trainNum)  # sample(x,y)函數的作用是從序列x中，隨機選擇y個不重複的元素
+    testresultList = random.sample(range(0, AllNum), testNum)  # sample(x,y)函數的作用是從序列x中，隨機選擇y個不重複的元素
 
     lines = All.readlines()
     for i in trainresultList:
@@ -80,25 +80,25 @@ def split_train_test():
 
 #
 # pass
-# #定义读取文件的格式
+# #定義讀取文件的格式
 
 
 class MnistDataset(Dataset):
     def __init__(self, image_path, image_label, transform=None, target_transform=None,
-                 loader=default_loader):  # __init__是初始化该类的一些基础参数
+                 loader=default_loader):  # __init__是初始化該類的一些基礎參數
         super(MnistDataset, self).__init__()
-        self.image_path = image_path  # 初始化图像路径列表
-        self.image_label = image_label  # 初始化图像标签列表
-        self.transform = transform  # 初始化数据增强方法
+        self.image_path = image_path  # 初始化圖像路徑列表
+        self.image_label = image_label  # 初始化圖像標簽列表
+        self.transform = transform  # 初始化數據增強方法
         self.target_transform = target_transform
         self.loader = loader
 
     def __getitem__(self, index):
         """
-        根据索引index返回dataset[index]
-        获取对应index的图像，并视情况进行数据增强
+        根據索引index返回dataset[index]
+        獲取對應index的圖像，並視情況進行數據增強
         """
-        # image = Image.open(self.image_path[index]).convert("RGB")  # Convert image to RGB mode if needed # 读取图像
+        # image = Image.open(self.image_path[index]).convert("RGB")  # Convert image to RGB mode if needed # 讀取圖像
         # # image = np.array(image)
         # label = float(self.image_label[index])
         img_name = self.image_path[index]
@@ -106,35 +106,35 @@ class MnistDataset(Dataset):
         image = self.loader(img_name)
 
         if self.transform is not None:
-            image = self.transform(image)  # 归一化到 [0.0,1.0]
+            image = self.transform(image)  # 歸一化到 [0.0,1.0]
         if self.target_transform is not None:
             label = self.target_transform(image)
         return image, label
 
-    def __len__(self):  # 返回整个数据集的大小
+    def __len__(self):  # 返回整個數據集的大小
         return len(self.image_path)
 
 
-# 全连接网络层 + 激活层 + BN 网络层
+# 全連接網絡層 + 激活層 + BN 網絡層
 
-# 定义分类模型
+# 定義分類模型
 
 
 class simpleNet(nn.Module):
     """
-    定义了一个简单的三层全连接神经网络，每一层都是线性的
+    定義了一個簡單的三層全連接神經網絡，每一層都是線性的
     """
 
     def __init__(self, in_dim, n_hidden_1, n_hidden_2, out_dim):
     # """
-    #     定义了一个简单的三层全连接神经网络，每一层都是线性的
+    #     定義了一個簡單的三層全連接神經網絡，每一層都是線性的
     # """
     #     super(simpleNet, self).__init__()
     #     self.layer1 = nn.Linear(in_dim, n_hidden_1)
     #     self.layer2 = nn.Linear(n_hidden_1, n_hidden_2)
     #     self.layer3 = nn.Linear(n_hidden_2, out_dim)
         super(simpleNet, self).__init__()
-        # 在上面的Activation_Net的基础上，增加了一个加快收敛速度的方法——批标准化
+        # 在上面的Activation_Net的基礎上，增加了一個加快收斂速度的方法——批標準化
         self.layer1 = nn.Sequential(nn.Linear(in_dim, n_hidden_1), nn.BatchNorm1d(n_hidden_1), nn.ReLU(True))
         self.layer2 = nn.Sequential(nn.Linear(n_hidden_1, n_hidden_2), nn.BatchNorm1d(n_hidden_2), nn.ReLU(True))
         self.layer3 = nn.Sequential(nn.Linear(n_hidden_2, out_dim))
@@ -172,17 +172,17 @@ def train(model, optimizer, train_loader, criterion, epoch):
 # to evaluate the model
 ## validation of test accuracy
 def val(model, criterion, val_loader, epoch, train=False):
-    model.eval()  # 在eval模式下，dropout层会让所有的激活单元都通过，而BN层会停止计算和更新mean和var，直接使用在训练阶段已经学出的mean和var值。
+    model.eval()  # 在eval模式下，dropout層會讓所有的激活單元都通過，而BN層會停止計算和更新mean和var，直接使用在訓練階段已經學出的mean和var值。
     val_loss = 0
     correct = 0
 
-    with torch.no_grad():  # 不记录模型梯度信息，以起到加速和节省显存的作用。它的作用是将该with语句包裹起来的部分停止梯度的更新
+    with torch.no_grad():  # 不記錄模型梯度信息，以起到加速和節省顯存的作用。它的作用是將該with語句包裹起來的部分停止梯度的更新
         for batch_idx, (data, target) in enumerate(val_loader):
             data = data.view(-1, 3 * 28 * 28).requires_grad_()
             output = model(data)
             val_loss += criterion(output, target).item()  # sum up batch loss
             pred = output.max(1, keepdim=True)[
-                1]  # get the index of the max log-probability， #通过predict和标签进行对比(predict是第几位是最大的概率, 标签是0 - 9的数字, 所以当predict和labels相等时就相当于预测值是正确的)来得到判断正确的数量并赋给correct
+                1]  # get the index of the max log-probability， #通過predict和標簽進行對比(predict是第幾位是最大的概率, 標簽是0 - 9的數字, 所以當predict和labels相等時就相當於預測值是正確的)來得到判斷正確的數量並賦給correct
             correct += pred.eq(target.view_as(pred)).sum().item()  # if pred == target then correct +=1
 
     val_loss /= len(val_loader.dataset)  # average val loss
@@ -200,7 +200,7 @@ def val(model, criterion, val_loader, epoch, train=False):
 def test(save_path, model, criterion, test_loader):
     if os.path.exists(save_path):
         loaded_paras = torch.load(save_path)
-        print("#### 成功载入已有模型，进行追加训练...")
+        print("#### 成功載入已有模型，進行追加訓練...")
         # 打印參數
         for param_tensor in model.state_dict():
             print(param_tensor, "\t", model.state_dict()[param_tensor].size())
@@ -247,7 +247,7 @@ def test(save_path, model, criterion, test_loader):
 def train_flod_Mnist(k_split_value, BATCH_SIZE, Iterations):
     different_k_mse = []
     kf = KFold(n_splits=k_split_value, shuffle=True,
-               random_state=0)  # init KFold，设置shuffle=True和random_state=整数，每次运行结果相同
+               random_state=0)  # init KFold，設置shuffle=True和random_state=整數，每次運行結果相同
     # split幾次與epochs數值相乘就是，小計訓練幾次，之後再看循環幾個split
     for train_index, val_index in kf.split(train_dataset):  # split
         # get train, val
@@ -275,10 +275,10 @@ def train_flod_Mnist(k_split_value, BATCH_SIZE, Iterations):
 
 def get_path_label(label_file_path):
     """
-    获取数字图像的路径和标签并返回对应的列表
-    @ img_root:保存图像的根目路
-    @ label_file_path 保存图像标签的文件路径，。cvs 或 。txt
-    @ return：图像的路径列表和对应的标签列表
+    獲取數字圖像的路徑和標簽並返回對應的列表
+    @ img_root:保存圖像的根目路
+    @ label_file_path 保存圖像標簽的文件路徑，。cvs 或 。txt
+    @ return：圖像的路徑列表和對應的標簽列表
     """
     data = pd.read_csv(label_file_path, names=['img', 'label'])
     print(data)
@@ -300,28 +300,29 @@ if __name__ == '__main__':
     # write_txt()
     # split_train_test()
     # exit()
-    # -------------------------------------超参数定义-------------------------------------
-    batch_size = 64  # 一个batch的size
+    # -------------------------------------超參數定義-------------------------------------
+    batch_size = 64  # 一個batch的size
     learning_rate = 0.01
-    num_epoches = 7  # 总样本的迭代次数
+    num_epoches = 7  # 總樣本的迭代次數
     fold_num = 4
 
-    # -------------------------------------选择模型--------------------------------------
+    # -------------------------------------選擇模型--------------------------------------
     model = simpleNet(3 * 28 * 28, 300, 100, 10)
     model.apply(init_weights)
     # print(model)
 
-    # -------------------------------------定义损失函数和优化器--------------------------------------
-    # -------------------------------------交叉熵和SGD优化器-------------------------------------
-    criterion = nn.CrossEntropyLoss()  # softmax与交叉熵一起
+    # -------------------------------------定義損失函數和優化器--------------------------------------
+    # -------------------------------------交叉熵和SGD優化器-------------------------------------
+    criterion = nn.CrossEntropyLoss()  # softmax與交叉熵一起
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     # -------------------------------------測試資料-------------------------------------
-    save_path = "C:/Users/mandy chou/Desktop/MNIST/perceptron5.pt"
-    # 获取测试集路径列表和标签列表
+
+    save_path = "C:/Users/mandy chou/Desktop/MNIST/version4_2/perceptron.pt"
+    # 獲取測試集路徑列表和標簽列表
     # 測試模型
-    test_label = './mnist/mnist/test.txt'  # 输入测试集的txt
+    test_label = './mnist/mnist/test_m.txt'  # 輸入測試集的txt
     test_img_list, test_label_list = get_path_label(test_label)
-    # 训练集dataset
+    # 訓練集dataset
     test_dataset = MnistDataset(test_img_list, test_label_list, transform=transforms.Compose([transforms.ToTensor()]))
     test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
     # #
@@ -331,15 +332,15 @@ if __name__ == '__main__':
 
     # -------------------------------------訓練與驗證-------------------------------------
     # 訓練與驗證
-    # 获取训练集路径列表和标签列表
-    train_label = './mnist/mnist/train.txt'  # 输入训练集的txt
+    # 獲取訓練集路徑列表和標簽列表
+    train_label = './mnist/mnist/train.txt'  # 輸入訓練集的txt
     train_img_list, train_label_list = get_path_label(train_label)
-    # 训练和测试集预处理
+    # 訓練和測試集預處理
 
     train_dataset = MnistDataset(train_img_list, train_label_list,
-                                 transform=transforms.Compose([transforms.ToTensor()]))  # 训练集dataset数据预处理方法
+                                 transform=transforms.Compose([transforms.ToTensor()]))  # 訓練集dataset數據預處理方法
 
-    # # 开始加载自己的数据
+    # # 開始加載自己的數據
     # train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
     # test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
     # print('num_of_trainData:', len(train_dataset))
@@ -355,7 +356,7 @@ if __name__ == '__main__':
     #     img, label = i
     #     print(img.size(), label)
 
-    # 设定K值为[2,10]进行训练
+    # 設定K值為[2,10]進行訓練
     valAcc_compare_map = {}
 
     for k_split_value in range(fold_num, fold_num + 2 + 1):
@@ -367,7 +368,7 @@ if __name__ == '__main__':
         # for j in range(k):  # j代表fold下的每次訓練與驗證結果，內容會是epcho，如果是fold4，則代表j會是0,1,2,3
         #     print(valAcc_compare_map[k][j])
         print('------------------')
-        arang_acc = int(np.mean(valAcc_compare_map[k]))
+        arang_acc = float(np.mean(valAcc_compare_map[k]))
         dict_new = {fold_num: arang_acc}
         dict.update(dict_new)
         fold_num += 1
@@ -376,3 +377,4 @@ if __name__ == '__main__':
         writer.writerow(['fold', 'avg_acc'])
         for k, v in dict.items():
             writer.writerow([k, v])
+
